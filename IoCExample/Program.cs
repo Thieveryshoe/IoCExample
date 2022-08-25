036a29a787d6ba598d139b7105fe2c7733d4ab8b
John Ellison <johnjellison@gmail.com>
2022-08-24 20:28:30 -0500

    removed commented code

--- IoCExample/Program.cs
+++ IoCExample/Program.cs
@@ -6,12 +6,6 @@ namespace Console
     {
         private static void Main()
         {
-            //var writer = new MessageWriter();
-            //var reader = new MessageReader();
-            //var app = new App(writer, reader);
-
-            //app.Execute();
-
             var container = Bootstrapper.Bootstrap();
 
             var app = container.GetInstance<IApp>();
